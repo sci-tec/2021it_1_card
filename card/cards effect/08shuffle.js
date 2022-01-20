fec7332079521bda135ba2df58af3b565b9fe447
A-N-Z-U <kinjouanzu2@gmail.com>
2022-01-20 09:46:17 +0900

    ファイルを追加しました

---
+++ card/cards effect/08shuffle.js
@@ -0,0 +1,24 @@
+import CONFIG from "../config.js"
+export default function(players) {
+    let tehuda = [];
+    let num = [];
+    // players[1].cards.pop()
+
+    for (let i = 0; i < CONFIG.NUM_PLAYERS; i++) {
+        num.push(players[i].cards.length)
+        console.log(players[i].cards)
+        tehuda.push(...players[i].cards)
+    }
+
+    for (let i = 0; i < CONFIG.NUM_PLAYERS; i++) {
+        players[i].cards = [];
+
+        for (let j = 0; j < num[i]; j++) {
+            let r = Math.floor(Math.random() * (tehuda.length - 1))
+            let card = tehuda.splice(r, 1)[0];
+            // console.log("card2", card)
+            // card.id = j;
+            players[i].cards.push(card);
+        }
+    }
+}
