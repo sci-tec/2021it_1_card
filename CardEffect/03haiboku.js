8e61d21899d96e95bd8dc007586d5215994e4916
A-N-Z-U <kinjouanzu2@gmail.com>
2022-02-03 15:00:23 +0900

    ファイルを更新しました。(07present.js)

---
+++ CardEffect/03haiboku.js
@@ -0,0 +1,8 @@
+import G from "./global.js";
+import CONFIG from "../config.js"
+
+export default function() {
+    
+    G.players[G.currentPlayer.id].winlose = "lose";
+
+}
--- CardEffect/07present.js
+++ CardEffect/07present.js
@@ -1,19 +1,57 @@
 import G from "../global.js";
 import CONFIG from "../config.js";
 
-export default function() {
-    const IRU = 0;
-    const IRANAI = 1;
-    G.getPlayersWithoutCurrentPlayer()
+export default function () {
 
-    let sentaku = prompt("半角で入力\n\n" + IRU + ":いる\n\n" + IRANAI + ":いらない")
+    G.coverAll();
+    
+    let nakami = ["cat", "snake"];
+    let r = Math.floor(Math.random() * nakami.length);
 
-    sentaku = parseInt(sentaku, 10)
-    console.log(sentaku)
+    let option = {
+        title: "プレゼント。「いる」か「いらない」か",
+        text: "「いる」か「いらない」かを選択して下さい",
+        options: [
+            "いる",
+            "いらない"
+        ]
+    }
 
-    if (sentaku == 0) {
-        alert("プレゼントは猫でした。かわいいね")
-    } else {
-        alert("プレゼントは猫でした。いらないを選んだ人は敗北。")
+    let answers = [];
+
+    G.getAnswers(option, (id, answerId) => {
+        answers.push({ id: id, answerId: answerId });
+        if (answers.length >= G.players.length) {
+            judge();
+        }
+    });
+
+    function judge() {
+
+        // audio.src='./mp3/out.mp3';
+        // audio.play();
+
+        answers.map((d, i) => {
+            if (nakami[r] == "cat") {
+                if (d.answerId == 1) {
+                    G.setCoverByPlayerId(i, true, { classes: "selected" })
+                    console.log(nakami[r])
+                    G.players[answers[i].id].winlose = "lose";
+                }
+            } else {
+                if (d.answerId == 0) {
+                    G.setCoverByPlayerId(i, true, { classes: "selected" })
+                    console.log(nakami[r])
+                    G.players[answers[i].id].winlose = "lose";
+                }
+            }
+        });
+        
+        console.log("playersInfo", G.players);
+
+        let tm = setInterval((e) => {
+            clearTimeout(tm);
+            G.hideCoverAll();
+        }, 4000);
     }
 }
